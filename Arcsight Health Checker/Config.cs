6781a6db0233b475c22dc988a69a36a26b2eef08
Emre Ekinci <yunusemrem@windowslive.com>
2019-08-26 22:33:06 +0300

    Add files via upload

---
+++ Arcsight Health Checker/Config.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Arcsight_Health_Checker {
+    public partial class Config {
+        [JsonProperty("loggerID")]
+        public string proxyAdress { get; set; }
+
+        [JsonProperty("loggerPass")]
+        public string proxyUsername { get; set; }
+
+        [JsonProperty("csirtMail")]
+        public string csirtMail { get; set; }
+
+        [JsonProperty("firefoxLoc")]
+        public string proxyAdress { get; set; }
+
+        [JsonProperty("loggerUrls")]
+        public List<string> ibmApiKeys { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+    }
+}
